d361302e09ca09c488a0a29c2b44094c2421929d
Weon Yuan <weon.yuan@gmail.com>
2016-04-28 16:26:38 -0400

    Backpatching works for temp entries in static table.

--- js/codeGenerator.js
+++ js/codeGenerator.js
@@ -11,9 +11,11 @@ var COMPILER;
         }
         CodeGenerator.build = function () {
             COMPILER.Main.addLog(LOG_INFO, 'Performing 6502a code generation.');
+            // Reset and initialize
             this.codeTable = [];
             this.staticTable = [];
             this.jumpTable = [];
+            this.currentIndex = 0;
             for (var i = 0; i < PROGRAM_SIZE; i++) {
                 this.codeTable[i] = '00';
             }
@@ -24,8 +26,10 @@ var COMPILER;
                 3. String assignment
             */
             this.generateCode(_AST.root);
-            console.log(this.codeTable);
+            // Set break statement
+            this.setCode('00');
             this.backpatch();
+            console.log(this.codeTable);
         };
         CodeGenerator.generateCode = function (node) {
             console.log(node);
@@ -48,22 +52,28 @@ var COMPILER;
             }
         };
         CodeGenerator.setCode = function (opcode) {
-            for (var i = 0; i < this.codeTable.length; i++) {
-                if (this.codeTable[i] === '00') {
-                    this.codeTable[i] = opcode;
-                    break;
-                }
+            // Set the opcode on the next available block
+            if (this.codeTable[this.currentIndex] === '00') {
+                this.codeTable[this.currentIndex] = opcode;
+                this.currentIndex++;
             }
+        };
+        CodeGenerator.injectCode = function (opcode, index) {
+            // Set the opcode on the target index of the code table
+            this.codeTable[index] = opcode;
         };
         CodeGenerator.handleVarDecl = function (node) {
             var dataType = node.children[0].name;
             var id = node.children[1].name;
+            // Initialize the id with '00'
             COMPILER.Main.addLog(LOG_VERBOSE, 'Generating declaration code for id ' + id + '.');
             this.setCode('A9');
             this.setCode('00');
+            // Create a temporary entry for the id
             var tempEntry = this.createTempEntry();
             tempEntry.id = id;
             // tempEntry.scope = 
+            // Store the accumulator value at the id's address
             this.setCode('8D');
             this.setCode(tempEntry.name);
             this.setCode('XX');
@@ -77,17 +87,32 @@ var COMPILER;
                 name: 'T' + this.staticTable.length,
                 id: '',
                 scope: 0,
-                addressOffset: 0
+                addressOffset: this.staticTable.length
             };
             this.staticTable.push(tempEntry);
             return tempEntry;
         };
         CodeGenerator.backpatch = function () {
             COMPILER.Main.addLog(LOG_VERBOSE, 'Backpatching the code.');
+            var staticStartIndex = this.currentIndex;
+            var currentCode = '';
+            var tempNameRegex = /^T.*/;
+            for (var i = 0; i < staticStartIndex; i++) {
+                currentCode = this.codeTable[i];
+                if (currentCode.match(tempNameRegex)) {
+                    var tempEntryIndex = parseInt(currentCode.substring(1));
+                    var tempEntry = this.staticTable[tempEntryIndex];
+                    var targetIndex = staticStartIndex + tempEntryIndex;
+                    console.log(targetIndex.toString(16));
+                    this.injectCode(targetIndex.toString(16), i++);
+                    this.injectCode('00', i);
+                }
+            }
         };
         CodeGenerator.codeTable = [];
         CodeGenerator.staticTable = [];
         CodeGenerator.jumpTable = [];
+        CodeGenerator.currentIndex = 0;
         return CodeGenerator;
     })();
     COMPILER.CodeGenerator = CodeGenerator;
--- ts/codeGenerator.ts
+++ ts/codeGenerator.ts
@@ -10,13 +10,16 @@ module COMPILER {
         public static codeTable: string[] = [];
         public static staticTable: any[] = [];
         public static jumpTable: any[] = [];
+        public static currentIndex: number = 0;
 
         public static build(): void {
             Main.addLog(LOG_INFO, 'Performing 6502a code generation.');
 
+            // Reset and initialize
             this.codeTable = [];
             this.staticTable = [];
             this.jumpTable = [];
+            this.currentIndex = 0;
 
             for (var i = 0; i < PROGRAM_SIZE; i++) {
                 this.codeTable[i] = '00';
@@ -29,8 +32,12 @@ module COMPILER {
                 3. String assignment
             */
             this.generateCode(_AST.root);
-            console.log(this.codeTable);
+
+            // Set break statement
+            this.setCode('00');
+
             this.backpatch();
+            console.log(this.codeTable);
         }
 
         public static generateCode(node): void {
@@ -59,26 +66,33 @@ module COMPILER {
         }
 
         public static setCode(opcode): void {
-            for (var i = 0; i < this.codeTable.length; i++) {
-                if (this.codeTable[i] === '00') {
-                    this.codeTable[i] = opcode;
-                    break;
-                }
+            // Set the opcode on the next available block
+            if (this.codeTable[this.currentIndex] === '00') {
+                this.codeTable[this.currentIndex] = opcode;
+                this.currentIndex++;
             }
+        }
+
+        public static injectCode(opcode, index): void {
+            // Set the opcode on the target index of the code table
+            this.codeTable[index] = opcode;
         }
 
         public static handleVarDecl(node): void {
             var dataType: string = node.children[0].name;
             var id: string = node.children[1].name;
             
+            // Initialize the id with '00'
             Main.addLog(LOG_VERBOSE, 'Generating declaration code for id ' + id + '.');
             this.setCode('A9');
             this.setCode('00');
 
+            // Create a temporary entry for the id
             var tempEntry = this.createTempEntry();
             tempEntry.id = id;
             // tempEntry.scope = 
 
+            // Store the accumulator value at the id's address
             this.setCode('8D');
             this.setCode(tempEntry.name);
             this.setCode('XX');
@@ -97,7 +111,7 @@ module COMPILER {
                 name: 'T' + this.staticTable.length,
                 id: '',
                 scope: 0,
-                addressOffset: 0
+                addressOffset: this.staticTable.length
             };
 
             this.staticTable.push(tempEntry);
@@ -107,6 +121,24 @@ module COMPILER {
 
         public static backpatch(): void {
             Main.addLog(LOG_VERBOSE, 'Backpatching the code.');
+
+            var staticStartIndex: number = this.currentIndex;
+            var currentCode: string = '';
+            var tempNameRegex = /^T.*/;
+
+            for (var i = 0; i < staticStartIndex; i++) {
+                currentCode = this.codeTable[i];
+
+                if (currentCode.match(tempNameRegex)) {
+                    var tempEntryIndex: number = parseInt(currentCode.substring(1));
+                    var tempEntry: any = this.staticTable[tempEntryIndex];
+
+                    var targetIndex: number = staticStartIndex + tempEntryIndex;
+                    console.log(targetIndex.toString(16));
+                    this.injectCode(targetIndex.toString(16), i++);
+                    this.injectCode('00', i);
+                }
+            }
         }
     }
 }
