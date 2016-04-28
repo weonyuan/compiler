9e8f9660e2bc16a4723d2355d07b48e77b908006
Weon Yuan <weon.yuan@gmail.com>
2016-04-28 00:57:02 -0400

    Working on code gen. Got var declaration to work. Need to resolve temp entries.

--- js/codeGenerator.js
+++ js/codeGenerator.js
@@ -12,15 +12,82 @@ var COMPILER;
         CodeGenerator.build = function () {
             COMPILER.Main.addLog(LOG_INFO, 'Performing 6502a code generation.');
             this.codeTable = [];
+            this.staticTable = [];
+            this.jumpTable = [];
             for (var i = 0; i < PROGRAM_SIZE; i++) {
                 this.codeTable[i] = '00';
             }
-            this.staticTable = [];
-            this.jumpTable = [];
+            /*
+                TODO:
+                1. Var Declaration
+                2. Int assignment
+                3. String assignment
+            */
+            this.generateCode(_AST.root);
+            console.log(this.codeTable);
+            this.backpatch();
         };
-        CodeGenerator.codeTable = null;
-        CodeGenerator.staticTable = null;
-        CodeGenerator.jumpTable = null;
+        CodeGenerator.generateCode = function (node) {
+            console.log(node);
+            switch (node.name) {
+                case 'Var Declaration':
+                    this.handleVarDecl(node);
+                    break;
+                case 'Assignment Statement':
+                    this.handleAssignmentStmt(node);
+                    break;
+                case 'Print Statement':
+                    this.handlePrintStmt(node);
+                    break;
+                default:
+                    // epsilon
+                    break;
+            }
+            for (var i = 0; i < node.children.length; i++) {
+                this.generateCode(node.children[i]);
+            }
+        };
+        CodeGenerator.setCode = function (opcode) {
+            for (var i = 0; i < this.codeTable.length; i++) {
+                if (this.codeTable[i] === '00') {
+                    this.codeTable[i] = opcode;
+                    break;
+                }
+            }
+        };
+        CodeGenerator.handleVarDecl = function (node) {
+            var dataType = node.children[0].name;
+            var id = node.children[1].name;
+            COMPILER.Main.addLog(LOG_VERBOSE, 'Generating declaration code for id ' + id + '.');
+            this.setCode('A9');
+            this.setCode('00');
+            var tempEntry = this.createTempEntry();
+            tempEntry.id = id;
+            // tempEntry.scope = 
+            this.setCode('8D');
+            this.setCode(tempEntry.name);
+            this.setCode('XX');
+        };
+        CodeGenerator.handleAssignmentStmt = function (node) {
+        };
+        CodeGenerator.handlePrintStmt = function (node) {
+        };
+        CodeGenerator.createTempEntry = function () {
+            var tempEntry = {
+                name: 'T' + this.staticTable.length,
+                id: '',
+                scope: 0,
+                addressOffset: 0
+            };
+            this.staticTable.push(tempEntry);
+            return tempEntry;
+        };
+        CodeGenerator.backpatch = function () {
+            COMPILER.Main.addLog(LOG_VERBOSE, 'Backpatching the code.');
+        };
+        CodeGenerator.codeTable = [];
+        CodeGenerator.staticTable = [];
+        CodeGenerator.jumpTable = [];
         return CodeGenerator;
     })();
     COMPILER.CodeGenerator = CodeGenerator;
--- ts/codeGenerator.ts
+++ ts/codeGenerator.ts
@@ -7,21 +7,106 @@
 
 module COMPILER {
     export class CodeGenerator {
-        public static codeTable: string[] = null;
-        public static staticTable: any[] = null;
-        public static jumpTable: any[] = null;
+        public static codeTable: string[] = [];
+        public static staticTable: any[] = [];
+        public static jumpTable: any[] = [];
 
         public static build(): void {
             Main.addLog(LOG_INFO, 'Performing 6502a code generation.');
 
             this.codeTable = [];
+            this.staticTable = [];
+            this.jumpTable = [];
 
             for (var i = 0; i < PROGRAM_SIZE; i++) {
                 this.codeTable[i] = '00';
             }
 
-            this.staticTable = [];
-            this.jumpTable = [];
+            /*
+                TODO:
+                1. Var Declaration
+                2. Int assignment
+                3. String assignment
+            */
+            this.generateCode(_AST.root);
+            console.log(this.codeTable);
+            this.backpatch();
+        }
+
+        public static generateCode(node): void {
+            console.log(node);
+            switch (node.name) {
+                case 'Var Declaration':
+                    this.handleVarDecl(node);
+
+                    break;
+                case 'Assignment Statement':
+                    this.handleAssignmentStmt(node);
+
+                    break;
+                case 'Print Statement':
+                    this.handlePrintStmt(node);
+
+                    break;
+                default:
+                    // epsilon
+                    break;
+            }
+
+            for (var i = 0; i < node.children.length; i++) {
+                this.generateCode(node.children[i]);
+            }
+        }
+
+        public static setCode(opcode): void {
+            for (var i = 0; i < this.codeTable.length; i++) {
+                if (this.codeTable[i] === '00') {
+                    this.codeTable[i] = opcode;
+                    break;
+                }
+            }
+        }
+
+        public static handleVarDecl(node): void {
+            var dataType: string = node.children[0].name;
+            var id: string = node.children[1].name;
+            
+            Main.addLog(LOG_VERBOSE, 'Generating declaration code for id ' + id + '.');
+            this.setCode('A9');
+            this.setCode('00');
+
+            var tempEntry = this.createTempEntry();
+            tempEntry.id = id;
+            // tempEntry.scope = 
+
+            this.setCode('8D');
+            this.setCode(tempEntry.name);
+            this.setCode('XX');
+        }
+
+        public static handleAssignmentStmt(node): void {
+
+        }
+
+        public static handlePrintStmt(node): void {
+
+        }
+
+        public static createTempEntry(): any {
+            var tempEntry: any = {
+                name: 'T' + this.staticTable.length,
+                id: '',
+                scope: 0,
+                addressOffset: 0
+            };
+
+            this.staticTable.push(tempEntry);
+
+            return tempEntry;
+        }
+
+        public static backpatch(): void {
+            Main.addLog(LOG_VERBOSE, 'Backpatching the code.');
         }
     }
 }
